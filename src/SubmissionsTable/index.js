d6a14216b225a669fe032f658e83ee45d5b79b7c
Trish Whetzel <plwhetzel@gmail.com>
2019-05-26 02:18:03 +0100

    update formatting, switch back to Container

--- src/SubmissionsTable/index.js
+++ src/SubmissionsTable/index.js
@@ -8,7 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 import APIClient from '../apiClient'
-import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 
 const useStyles = makeStyles(theme => ({
@@ -100,11 +100,11 @@ class SubmissionTable extends React.Component {
 
     render() {
         return (
-            <Grid style={{ margin: 20 }}>
+            <Container style={{ marginTop: 20 }}>
                 <div>
                     {this.renderRepos(this.state.kudos)}
                 </div>
-            </Grid>
+            </Container>
         );
     }
 }
