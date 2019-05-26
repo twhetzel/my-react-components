efbbee60e3e369a153292e2951b7145d0188c66c
Trish Whetzel <plwhetzel@gmail.com>
2019-05-26 01:38:01 +0100

    first pass at displaying data in table from web service

--- src/SubmissionsTable/index.js
+++ src/SubmissionsTable/index.js
@@ -7,6 +7,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import APIClient from '../apiClient'
+import { Container } from '@material-ui/core';
+
+
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
@@ -18,50 +22,91 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+// const classes = useStyles();
+
 function createData(pmid, title, file_valid_status, data_valid_status, curator) {
     return { pmid, title, file_valid_status, data_valid_status, curator };
 }
 
 const rows = [
     createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
-    createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
-    createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
-    createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
-    createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
-    createData('PMID:1234', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
+    createData('PMID:5', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
+    createData('PMID:6', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
+    createData('PMID:7', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
+    createData('PMID:8', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
+    createData('PMID:8', 'Super GWAS paper', 'In Progress', 'In Progress', 'Test Curator'),
 ];
 
-function SubmissionsTable() {
-    const classes = useStyles();
 
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>PMID</TableCell>
-                        <TableCell align="center">Title</TableCell>
-                        <TableCell align="center">File Validation Status</TableCell>
-                        <TableCell align="center">Data Validation Status</TableCell>
-                        <TableCell align="center">Curator</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => (
-                        <TableRow key={row.pmid}>
-                            <TableCell component="th" scope="row">
-                                {row.pmid}
-                            </TableCell>
-                            <TableCell align="center">{row.title}</TableCell>
-                            <TableCell align="center">{row.file_valid_status}</TableCell>
-                            <TableCell align="center">{row.data_valid_status}</TableCell>
-                            <TableCell align="center">{row.curator}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+// function SubmissionsTable() {
+class SubmissionTable extends React.Component {
+    // const classes = useStyles();
+
+    state = {
+        value: 0,
+        repos: [],
+        kudos: []
+    };
+
+    async componentDidMount() {
+        // const accessToken = await this.props.auth.getAccessToken()
+        // this.apiClient = new APIClient(accessToken);
+        this.apiClient = new APIClient();
+        this.apiClient.getKudos().then((data) =>
+            this.setState({ ...this.state, kudos: data })
+        );
+    }
+
+
+
+    renderRepos = (repos) => {
+        console.log(repos.allSubmissions)
+
+        if (!repos) { return [] }
+
+        if (repos.allSubmissions !== undefined) {
+            return (
+                // <Paper className={classes.root}>
+                <Paper>
+                    {/* <Table className={classes.table}> */}
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>PMID</TableCell>
+                                <TableCell align="center">Title</TableCell>
+                                <TableCell align="center">File Validation Status</TableCell>
+                                <TableCell align="center">Data Validation Status</TableCell>
+                                <TableCell align="center">Curator</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {repos.allSubmissions.map(row => (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.publication_id}
+                                    </TableCell>
+                                    <TableCell align="center">{row.publication_id}</TableCell>
+                                    <TableCell align="center">{row.is_valid_format}</TableCell>
+                                    <TableCell align="center">{row.is_valid_data}</TableCell>
+                                    <TableCell align="center">{row.user_id}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <div>
+                    {this.renderRepos(this.state.kudos)}
+                </div>
+            </Container>
+        );
+    }
 }
 
-export default SubmissionsTable;
+export default (SubmissionTable);
