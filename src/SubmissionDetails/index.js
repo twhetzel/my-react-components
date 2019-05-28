2adba442838351023907fb47c1d8f04dcf9cf621
Trish Whetzel <plwhetzel@gmail.com>
2019-05-28 23:49:15 +0100

    one option to display new component on button click

--- src/SubmissionDetails/index.js
+++ src/SubmissionDetails/index.js
@@ -6,10 +6,12 @@ import { Button } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Link, withRouter, Redirect } from 'react-router-dom';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 // import './SubmissionDetails.css'
-import Submissions from '../Submissions';
+import Test from '../Test';
 
 // import PropTypes from 'prop-types'
 // import { browserHistory } from 'react-router';
+
+import TestPage from '../Test';
 
 const styles = theme => ({
     root: {
@@ -27,11 +29,43 @@ const styles = theme => ({
 });
 
 
-const AddTripButton = props => {
-    return <button onClick={props.addTrip}>Add a trip</button>
-}
+// const AddTripButton = props => {
+//     return <button onClick={props.addTrip}>Add a trip</button>
+// }
 
 class SubmissionDetails extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showComponent: false,
+        };
+        this._onButtonClick = this._onButtonClick.bind(this);
+    }
+
+    _onButtonClick() {
+        this.setState({
+            showComponent: true,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this._onButtonClick}>Button</Button>
+                {this.state.showComponent ?
+                    <Test /> :
+                    null
+                }
+            </div>
+        );
+    }
+
+
+
+
+
+
+
     // constructor(props) {
     //     super(props)
 
@@ -52,30 +86,64 @@ class SubmissionDetails extends React.Component {
     //     console.log(this.isButtonClicked)
     // }
 
-    constructor(props) {
-        super(props)
-        this.state = { isEmptyState: true }
-    }
-    triggerAddTripState = () => {
-        this.setState({
-            ...this.state,
-            isEmptyState: false,
-            isAddTripState: true
-        })
-    }
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { isEmptyState: true }
+    // }
+    // triggerAddTripState = () => {
+    //     this.setState({
+    //         ...this.state,
+    //         isEmptyState: false,
+    //         isAddTripState: true
+    //     })
+    // }
 
-    render() {
-        return (
-            <Container style={{ marginTop: 20 }}>
-                <div>
-                    SubmissionID: {this.props.submissionID}
-                </div>
-                <Button color="secondary" onClick={this.triggerAddTripState}>
-                    Upload File
-                    </Button>
-            </Container>
-        )
-    }
+    // PageView() {
+    //     if (this.state.isEmptyState) {
+    //         return (
+    //             <Container style={{ marginTop: 20 }}>
+    //             <div>
+    //                 SubmissionID: {this.props.submissionID}
+    //             </div>
+    //             <Button color="secondary" onClick={this.triggerAddTripState}>
+    //                 Upload File
+    //                 </Button>
+    //         </Container>
+    //         )
+    //     }
+    //     if (this.state.isAddTripState) {
+    //         <Submissions />
+    //     }
+
+    // }
+
+
+    // render() {
+    //     console.log(this.state.isAddTripState)
+    //     console.log(this.state.isEmptyState)
+
+    //     if (this.state.isEmptyState) {
+    //         return (
+    //             // {this.state.isEmptyState && <AddTripButton addTrip={this.triggerAddTripState} />}
+    //             <Container style={{ marginTop: 20 }}>
+    //                 <div>
+    //                     SubmissionID: {this.props.submissionID}
+    //                 </div>
+    //                 <Button color="secondary" onClick={this.triggerAddTripState}>
+    //                     Upload File
+    //                 </Button>
+    //             </Container>
+    //         )
+    //     }
+
+    //     if (this.state.isAddTripState) {
+    //         return (
+    //             <div>
+    //                 {this.state.isAddTripState && <Submissions />}
+    //             </div>
+    //         )
+    //     }
+    // }
 }
 
 
